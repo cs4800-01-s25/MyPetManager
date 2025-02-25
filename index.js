215c22199dae47fdc96bf658967925bd3cca09fb
Gian David Marquez <giandmarquez@gmail.com>
2025-02-25 00:19:10 -0800

    Added express and concurrently node.js packages for backend

--- index.js
+++ index.js
@@ -6,31 +6,35 @@
 // Example from nodejs.org
 // Ideally package.json would concurrently run backend and the frontend
 // This is just a test on trying a simple server and starting through 'npm start'
+// Looking back, I needed the express.js to simplify routing
+// This index.js is typically also named app.js
+
 
 // Define requirements;
+const express = require( "express" );
 const domain = "localhost";
 const port = 3000;
 const appName = "TEST";
 
-const { createServer } = require("node:http");
+const app = express();
 
 // add ctrl C to escape
 process.on( 'SIGINT', () => {
     console.log('SIGINT' );
     server.close( () => {
-        console.log( `The $(appName) server will stop now.`);
+        console.log( `The ${appName} server will stop now.`);
     } );
     process.exitCode = 0;
 } );
 
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+// Example of HTTP GET / REQUEST
+app.get('/', (req, res) => {
+  res.send('Hello World EXPRESS');
 });
 
-server.listen(port, domain, () => {
-  console.log(`Server running at http://${domain}:${port}/`);
+
+var server = app.listen(port, domain, () => {
+  console.log(`Server running express at http://${domain}:${port}/`);
 });
 
