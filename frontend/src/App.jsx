e73acc94425c4846d54a38a1de241c7dfc217cee
Gian David Marquez <giandmarquez@gmail.com>
2025-03-19 22:14:03 -0700

    basic log-in page for testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import HomePage from './pages/HomePage';
+import HomePage from './pages/HomePage'; // is it's not defaulting to homepage: showing nothing.  
+import LoginPage from './pages/LoginPage'; // safe
 
 function App() {
   return (
@@ -19,14 +20,15 @@ function App() {
               <Link to="/" style={{ color: '#5A7184', fontWeight: 'bold' }}>Home</Link>
             </li>
             <li>
-              <Link to="/log-in" style={{ color: '#5A7184', fontWeight: 'bold' }}>Log In</Link>
+              <Link to="/login" style={{ color: '#5A7184', fontWeight: 'bold' }}>Login</Link>
             </li>
           </ul>
         </nav>
 
         {/* Routes */}
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />  
+          <Route path="/login" element={<LoginPage />} />    
         </Routes>
       </div>
     </Router>
