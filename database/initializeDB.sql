03de64d15ee3b8d5ca4dbed0ada330557c8f9379
Gian David Marquez <giandmarquez@gmail.com>
2025-04-30 20:35:42 -0700

    added pet photos in db

--- database/initializeDB.sql
+++ database/initializeDB.sql
@@ -37,7 +37,6 @@ CREATE TABLE pets (
     Species VARCHAR(50),
     Sex ENUM('Female', 'Male', 'Other') NOT NULL,
     Weight DECIMAL(5, 2),
-    PetPhotoURL VARCHAR(2048) DEFAULT NULL,
     CreatedAt TIMESTAMP NOT NULL DEFAULT NOW(),
     FOREIGN KEY (PetOwnerID) REFERENCES petowners (PetOwnerID) ON DELETE CASCADE
 );
@@ -61,4 +60,27 @@ INDEX idx_app_pet (PetID),
 INDEX idx_app_owner (PetOwnerID),
 INDEX idx_app_datetime (AppointmentDateTime),
 INDEX idx_app_status (Status)
+);
+
+CREATE TABLE photos (
+    PhotoID INT AUTO_INCREMENT PRIMARY KEY,
+    -- PICK BETWEEN USER OR PET
+    UserID INT DEFAULT NULL,
+    PetID INT DEFAULT NULL,
+    S3Key VARCHAR(255) NOT NULL UNIQUE, 
+    -- IMPLEMENT FOR IF MULTIPLE PHOTOS CAN BE STORED
+    IsPrimary BOOLEAN NOT NULL DEFAULT FALSE,
+    -- image/jpeg vs image/png
+    ContentType VARCHAR(100),
+    UploadedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+-- if pet or user is deleted, automatically delete the photo in db    
+FOREIGN KEY (UserID) REFERENCES users (UserID) ON DELETE CASCADE,
+FOREIGN KEY (PetID) REFERENCES pets (PetID) ON DELETE CASCADE,
+
+-- Indexes for faster lookups based on linked entities
+INDEX idx_photo_user (UserID),
+INDEX idx_photo_pet (PetID),
+INDEX idx_photo_primary_user (UserID, IsPrimary), 
+INDEX idx_photo_primary_pet (PetID, IsPrimary) 
 );
